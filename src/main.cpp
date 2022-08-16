5156647f0c8c80b066d205faf54648da0ed400aa
Fabian Sauter <sauter.fabian@mailbox.org>
2022-08-16 14:39:41 +0200

    Added a headless mode via --headless

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include "logger/Logger.hpp"
 #include "sim/Simulator.hpp"
 #include "ui/UiContext.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
 #include <memory>
+#include <thread>
 
-int main(int argc, char** argv) {
-    logger::setup_logger(spdlog::level::debug);
-    SPDLOG_INFO("Launching Version: {} {}", MOVEMENT_SIMULATOR_VERSION, MOVEMENT_SIMULATOR_VERSION_NAME);
+bool should_run_headless(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        // NOLINTNEXTLINE (cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        if (std::strcmp(argv[i], "--headless") == 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
+int run_headless() {
+    SPDLOG_INFO("Launching Version {} {} in headless mode.", MOVEMENT_SIMULATOR_VERSION, MOVEMENT_SIMULATOR_VERSION_NAME);
+    std::shared_ptr<sim::Simulator> simulator = sim::Simulator::get_instance();
+    simulator->start_worker();
+
+    simulator->continue_simulation();
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    simulator->stop_worker();
+    return EXIT_SUCCESS;
+}
+
+int run_ui(int argc, char** argv) {
+    SPDLOG_INFO("Launching Version {} {} in UI mode.", MOVEMENT_SIMULATOR_VERSION, MOVEMENT_SIMULATOR_VERSION_NAME);
     std::shared_ptr<sim::Simulator> simulator = sim::Simulator::get_instance();
     simulator->start_worker();
 
@@ -17,4 +43,14 @@ int main(int argc, char** argv) {
 
     simulator->stop_worker();
     return result;
+}
+
+int main(int argc, char** argv) {
+    logger::setup_logger(spdlog::level::debug);
+    bool headless = should_run_headless(argc, argv);
+
+    if (headless) {
+        return run_headless();
+    }
+    return run_ui(argc, argv);
 }
