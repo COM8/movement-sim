727581148ce3008e775e9ad4c82b867c96107e0f
Fabian Sauter <sauter.fabian@mailbox.org>
2022-08-25 14:10:31 +0200

    Plotting GPU data

--- hardware_stats/src/plot.py
+++ hardware_stats/src/plot.py
@@ -1,16 +1,16 @@
 from matplotlib.axes import Axes
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Dict
 from datetime import datetime
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.animation import FuncAnimation
 
-# plt.style.use('fivethirtyeight')
+# nvidia-smi --query-gpu=timestamp,name,utilization.gpu,power.draw --format=csv -lms 100 -f dataGpu.txt
 
-def parse_results(filePath: str) -> List[Tuple[datetime, float, float, float, float, float, float, float, float]]:
-    print("Parsing data...")
-    results: List[Tuple[str, float, float, float, float, float, float, float, float, float, float]] = list()
+def parse_cpu_results(filePath: str) -> List[Tuple[datetime, float, float, float, float, float, float, float, float]]:
+    print("Parsing CPU data...")
+    results: List[Tuple[datetime, float, float, float, float, float, float, float, float]] = list()
     try:
         with open(filePath) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
@@ -25,6 +25,27 @@ def parse_results(filePath: str) -> List[Tuple[datetime, float, float, float, fl
     except Exception as e:
         print("Opening failed: " + str(e))
     return results
+
+def parse_gpu_results(filePath: str) -> List[Tuple[datetime, float, float, float, float, float, float, float, float]]:
+    print("Parsing GPU data...")
+    resultsDict: Dict[str, List[Tuple[datetime, float, float]]] = dict()
+    try:
+        with open(filePath) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader, None)
+            for row in csv_reader:
+                if len(row) == 4:
+                    try:
+                        tp: datetime = datetime.strptime(row[0], "%Y/%m/%d %H:%M:%S.%f")
+                        name: str = row[1]
+                        if not name in resultsDict:
+                            resultsDict[name] = list()
+                        resultsDict[name].append((tp, float(row[2].replace(" %", "")), float(row[3].replace(" W", ""))))
+                    except Exception as e:
+                        print("Parsing failed: " + str(e))
+    except Exception as e:
+        print("Opening failed: " + str(e))
+    return list(resultsDict.values())
 
 def plot_all_cpus(ax: Axes, dataBetter: List[List[Union[datetime, float]]]):
     ax.cla()
@@ -86,18 +107,73 @@ def plot_cpus(ax: Axes, dataBetter: List[List[Union[datetime, float]]]):
     secondAx.set_ylabel("Utilization")
     secondAx.legend(loc='upper right')
 
+def plot_all_gpus(ax: Axes, dataBetter: List[List[Union[datetime, float]]]):
+    ax.cla()
+    ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation = -90)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    ax.xaxis.set_major_locator(mdates.SecondLocator(interval=2))
+    ax.set_xlim([min(dataBetter[0][0]), max(dataBetter[0][0])])
+    ax.set_ylim([0, 200])
+
+    ax.plot(dataBetter[0][0], dataBetter[0][2], linewidth=1.0, label=f'GPU0 (max: {round(max(dataBetter[0][2]), 2)})')
+    ax.plot(dataBetter[1][0], dataBetter[1][2], linewidth=1.0, label=f'GPU1 (max: {round(max(dataBetter[1][2]), 2)})')
+    ax.legend(loc='upper left')
+
+    secondAx: Axes = ax.twinx()
+    secondAx.cla()
+    secondAx.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    secondAx.xaxis.set_major_locator(mdates.SecondLocator(interval=2))
+    secondAx.set_ylim([0, 100])
+    secondAx.set_xlim([min(dataBetter[0][0]), max(dataBetter[0][0])])
+    secondAx.plot(dataBetter[0][0], dataBetter[0][1], linewidth=1.0, label=f'GPU0 Utilization (max: {round(max(dataBetter[0][1]), 2)}%)')
+    secondAx.plot(dataBetter[1][0], dataBetter[0][1], linewidth=1.0, label=f'GPU1 Utilization (max: {round(max(dataBetter[1][1]), 2)}%)')
+    secondAx.set_ylabel("Utilization")
+    secondAx.legend(loc='upper right')
+
+def plot_gpus(ax: Axes, dataBetter: List[List[Union[datetime, float]]]):
+    ax.cla()
+    ax.clear()
+    ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation = -90)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    ax.xaxis.set_major_locator(mdates.SecondLocator(interval=2))
+
+    sumWatt: List[float] = [sum(value) for value in zip(dataBetter[0][2], dataBetter[1][2])]
+    gpuUtilization: List[float] = [sum(value) / 2 for value in zip(dataBetter[0][1], dataBetter[1][1])]
+
+    ax.plot(dataBetter[0][0], sumWatt, linewidth=1.0, label=f'GPU (max: {round(max(sumWatt), 2)})')
+    ax.set_xlim([min(dataBetter[0][0]), max(dataBetter[0][0])])
+    ax.set_ylim([0, 400])
+    ax.set_ylabel("Watt")
+    ax.set_xlabel("Time")
+    ax.legend(loc='upper left')
+
+    secondAx: Axes = ax.twinx()
+    secondAx.cla()
+    secondAx.clear()
+    secondAx.xaxis.set_major_formatter(mdates.DateFormatter('%M:%S'))
+    secondAx.xaxis.set_major_locator(mdates.SecondLocator(interval=2))
+    secondAx.set_ylim([0, 100])
+    secondAx.set_xlim([min(dataBetter[0][0]), max(dataBetter[0][0])])
+    secondAx.plot(dataBetter[0][0], gpuUtilization, linewidth=1.0, label=f'Utilization (max: {round(max(gpuUtilization), 2)}%)')
+    secondAx.set_ylabel("Utilization")
+    secondAx.legend(loc='upper right')
+
 def animate(i, ax):
     lastN: int = -250
-    data = parse_results("build/src/data.txt")[lastN:]
-    if len(data) <= 0:
-        return
-    dataBetter: List[List[Union[datetime, float]]] = list(zip(*data))
+    dataCpu = parse_cpu_results("build/src/dataCpu.txt")[lastN:]
+    if len(dataCpu) > 0:
+        dataCpuBetter: List[List[Union[datetime, float]]] = list(zip(*dataCpu))
+        plot_all_cpus(ax[0][0], dataCpuBetter)
+        plot_cpus(ax[0][1], dataCpuBetter)
 
-    plot_all_cpus(ax[0], dataBetter)
-    plot_cpus(ax[1], dataBetter)
+    dataGpu = parse_gpu_results("build/src/dataGpu.txt")
+    if len(dataGpu) == 2 and len(dataGpu[0]) > 0 and len(dataGpu[0]) == len(dataGpu[1]):
+        dataGpuBetter: Tuple[List[List[Union[datetime, float]]], List[List[Union[datetime, float]]]] = (list(zip(*(dataGpu[0][lastN:]))), list(zip(*(dataGpu[1][lastN:]))))
+        plot_all_gpus(ax[1][0], dataGpuBetter)
+        plot_gpus(ax[1][1], dataGpuBetter)
 
 
-fig, ax = plt.subplots(2, 1, constrained_layout=True)
+fig, ax = plt.subplots(2, 2, constrained_layout=True)
 fig.suptitle("CPU performance statistics", fontsize=16)
 ani = FuncAnimation(fig, animate, interval=250, fargs=(ax,))
 plt.show()
