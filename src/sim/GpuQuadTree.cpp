9af237a1295ea3f86ace111de7166a8fbf9cb736
Fabian Sauter <sauter.fabian@mailbox.org>
2022-10-03 13:52:52 +0200

    Simplified quadtree #level calculation

--- src/sim/GpuQuadTree.cpp
+++ src/sim/GpuQuadTree.cpp
@@ -10,8 +10,8 @@ void init_level_zero(Level& level, float worldSizeX, float worldSizeY) {
 }
 
 size_t calc_level_count(size_t maxDepth) {
-    size_t result = static_cast<size_t>(std::pow(4, maxDepth - 1));
-    for (size_t i = 0; i < (maxDepth - 1); i++) {
+    size_t result = 0;
+    for (size_t i = 0; i < maxDepth; i++) {
         result += static_cast<size_t>(std::pow(4, i));
     }
     return result;
